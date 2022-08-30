d2b6e86985de9ca4984792d95fe36353b6827c9e
Vinícius Alves <91344917+ViniiAlves@users.noreply.github.com>
2022-08-30 12:25:05 -0300

    pra testar o comando time

--- 03-back-end/bloco-22/22-02-fluxo-assíncrono/pokemon.js
+++ 03-back-end/bloco-22/22-02-fluxo-assíncrono/pokemon.js
@@ -1,8 +1,15 @@
 const axios = require('axios');
 
+const getPokemonById = async (id) => {
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  return response.data;
+}
+
 async function main(){
-  const response = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
-  console.log(response.data.name);
+  for (let i = 1; i <= 50; i++) {
+    const pokemon = await getPokemonById(i);
+    console.log(pokemon.name);
+  }
 }
 
 main();
